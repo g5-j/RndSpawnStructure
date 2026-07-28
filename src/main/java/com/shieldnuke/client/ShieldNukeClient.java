ce89ff65029f0a401a33d5dc3f62f19433ce1fb9
ABOOD <abdovop0@gmail.com>
2026-07-28 11:37:39 +0200

    Create ShieldNukeClient.java

---
+++ src/main/java/com/shieldnuke/client/ShieldNukeClient.java
@@ -0,0 +1,9 @@
+package com.shieldnuke.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShieldNukeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
