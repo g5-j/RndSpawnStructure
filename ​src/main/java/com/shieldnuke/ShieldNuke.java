f510fef905a57e84a816f0d24fbba3f35c0d80ef
ABOOD <abdovop0@gmail.com>
2026-07-28 11:37:07 +0200

    Create ShieldNuke.java

---
+++ ​src/main/java/com/shieldnuke/ShieldNuke.java
@@ -0,0 +1,11 @@
+package com.shieldnuke;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ShieldNuke implements ModInitializer {
+    public static final String MOD_ID = "shieldnuke";
+
+    @Override
+    public void onInitialize() {
+    }
+}
