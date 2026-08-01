93fed4e1b2292588f41809db1b07f9ebd9e315e1
ABOOD <abdovop0@gmail.com>
2026-08-01 07:42:34 +0200

    Delete src/main/java/com/shieldnuke directory

--- src/main/java/com/shieldnuke/client/ShieldNukeClient.java
+++
@@ -1,9 +0,0 @@
-package com.shieldnuke.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class ShieldNukeClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
